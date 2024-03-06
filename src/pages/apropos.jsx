e29cc2d9c3acfafb1b8c448d0aa060aa93324b4c
alixfanny <alix.fanny.mail@gmail.com>
2024-03-06 19:58:53 +0100

    modification texte a propos

--- src/pages/apropos.jsx
+++ src/pages/apropos.jsx
@@ -34,36 +34,38 @@ function Apropos() {
                 </div>
                 <div className='texte'>
                     <h3>Ma Passion : la BBE - La Beauté et le Bien Être intérieur et extérieur. </h3>
-                    <p>Mes centres d'intérêts sont le sport et l'adrénaline, les animaux, la nature, les voyages, l'art, l'élégance, la beauté, la psychologie, l'aménagement, la décoration d'intérieur, le fengshui, la lithothérapie.<br/>
-                        J'ai créé la BBE pour résumer le lien qui relie mon monde intérieur à mon monde extérieur, c'est-à-dire l'attirance pour la beauté et la recherche constante d'un bien être intérieur et extérieur.
-                        J'ai baigné très jeune dans les soins énergétiques de l'être humain et de l'habitat. Mon arrière-grand-mère était la guérisseuse de son village en Algérie et j'observais, petite, ma mère effectuer des soins énergétiques à la maison.
+                    <p>Dite hypersensible, je ressens plus fort, plus intensément, ce qui m'entoure et ce qui m'habite, agréable ou non.
+Parfois difficile à canaliser, cette particularité reste un don qui me donne accès à un monde qui pour un grand nombre est invisible. 
+Je dédie ma vie à faire croître ce qui m'anime et qui me donne de la vie. Je tends à croire que suivre ses envies nous maintient en vie... 
+Lorsqu'on côtoie les ténèbres au point de ne plus ressentir de joie à l'intérieur de soi, on comprend qu'il est vital de s'accrocher à la vie en recherchant au fond de soi ce qui nous anime depuis toujours ❤️ 
+                    </p>
+                    <p>En regardant en arrière, les shootings et défilés font parties des activités qui m'accompagnent depuis mes débuts sur Terre et qui me procurent beaucoup de joie.<br/>
+                    Et en regardant maintenant, je constate que :
                     </p>
                     <p>
-                        L'éveil de conscience, que nous vivons tous, amène une montée en croissance de personnes attirées par les différentes méthodes de bien-être et de soins énergétiques. Je suis honorée et heureuse de pourvoir partager tout ça et également d'en découvrir un peu plus chaque jour. 
+                    ❤️ J'aime ce qui est lié au bien être intérieur et extérieur : le fengshui, la lithothérapie, la psychologie, la décoration d'intérieur, 
                     </p>
                     <p>
-                        "Le Moi s'élève à la grâce d'un Toi<br/>
-                        Ta perception de Moi est un reflet de Toi<br/>
-                        Ma réaction à Toi est une sensibilité en Moi" 
+                    ❤️ J'aime la beauté en soi et à l'extérieur de soi : l'art, l'élégance, la sensibilité, la mode, les lieux d'exceptions, la solidarité, les défilés hautes coutures, les voyages, la musique, le partage.
                     </p>
-                    <p>Depuis petite l'être humain me touche profondément. Mon envie de le comprendre et d'améliorer sa qualité de vie fait partie de moi :
-                        "Bisounours, trop gentille, coach, innocente, psychologue, vieille âme" peu importe le mot qu'on peut utiliser pour parler de mon don la bienveillance !
-                        Je me sens bien quand je sens que je fais du bien tout en me faisant du bien également. J'aime la beauté et l'élégance, qu'elle soit intérieure et extérieure.
-                        Le lien social est important pour moi et nous sommes tous là pour nous élever les uns les autres, avoir une meilleure compréhension de soi-même à travers l'autre.
+                    <p>
+                    ❤️ J'aime tout ce qui me donne de l'énergie et m'aide à devenir une meilleure personne chaque jour : les animaux, les challenges, la nature, les voyages, le sport, l'adrénaline.
                     </p>
-                    <p>Aujourd'hui et après un gros travail d'introspection, je considère que j'ai la maturité nécessaire pour savoir ce à quoi j'aspire dans le domaine de la mode.
-                        Mon esprit scientifique fera toujours partie de moi et me pousse au quotidien à comprendre ce qui m'entoure et à donner du sens à mes actions.
+                    <p>
+                    
                     </p>
                 </div>
             </section>
 
             <div className='liste'>
                 <p>Le dénominateur commun de chacune de mes expériences est la BBE, c'est pourquoi :</p>
-                <ul>
-                    <li>Ma personnalité d'ingénieur et de décoratrice d'intérieur continue d'exercer à travers le blog Instagram TMH <a href="https://www.instagram.com/toit_et_moi_en_harmonie/" target="_blank" rel="noreferrer" className='lien'>@Toit_et_moi_en_Harmonie</a> dont l'objectif et de partager des outils et des méthodes de bien-être chez Soi et en Soi. TMH c'est le lien subtil entre notre monde intérieur et notre monde extérieur. L'un existe parce que l'autre le crée.</li>
-                    <li>Ma personnalité de Top Model s'épanouie à travers les shootings et défilés en lien avec la BBE que vous pouvez voir sur le book en ligne et sur le portfolio Instagram <a href="https://www.instagram.com/scheherazade_portfolio/" target="_blank" rel="noreferrer" className='lien'>@scheherazade_portfolio.</a></li>
-                    <li>Et pour mon plus grand bonheur, ma moitié et ma chienne Harmonie, que vous voyez sur les photos, m'accompagne dans mes différents projets.</li>
-                </ul>
+                <p>❤️ Ma personnalité de Top Model s'épanouie à travers les shootings et défilés depuis mon plus jeune âge.
+Quand je défile ou que je suis en shooting, je me sens bien. C'est d'ailleurs pour ça que je l'ai longtemps considéré comme un loisir.
+Même si parfois je peux ressentir du stress ou ne pas être à l'aise, parce que j'ai l'envie de bien faire, c'est une activité qui me donne de l'énergie, du plaisir, qui m'inspire et nourrit ma créativité.  </p>
+                <p>❤️ Ma personnalité d'ingénieur et de décoratrice d'intérieur, de par ma formation, continue de s'amuser à travers le blog Instagram TMH @Toit_et_moi_en_Harmonie dont l'objectif et de partager des outils, des méthodes de bien-être chez Soi et en Soi et également de mettre en avant des artistes et ouvriers de lumières. 
+TMH c'est le lien subtil entre notre monde intérieur et notre monde extérieur. L'un existe parce que l'autre le crée. </p>
+                <p>❤️ Et pour mon plus grand bonheur, ma moitié et ma chienne Harmonie, que vous voyez sur les photos, m'accompagne dans mes différents projets. </p>
+                <p>J'ai donc créé la BBE pour résumer le lien qui relie mon monde intérieur à mon monde extérieur, c'est-à-dire l'attirance profonde pour la beauté et la recherche constante d'un bien être intérieur et extérieur.</p>
             </div>
 
             <div className='line'></div>
@@ -71,19 +73,19 @@ function Apropos() {
             <section  className="section">
                 <div className='texte'>
                     <h3>Mes Projets : Œuvrer pour la BBE à plus grande échelle.</h3>
-                    <p>J'ai à cœur de pouvoir mettre en lumière les sujets qui me touchent à travers mes différents projets.</p>
-                    <p>C'est une manière pour moi de sentir que je contribue à l'évolution de notre planète Terre.
-                        C'est en quelque sorte ma façon de remercier la vie pour toute la beauté qui réside en moi et autour de moi, c'est ce que j'ai à cœur d'exprimer où que j'aille, quoique je fasse.
-                    </p>
-                    <p>C'est aussi une manière de mettre en valeur mon identité, mon Moi intérieur et non me conformer à ce que 'je crois qu'il serait bien d'être".</p>
-                    <p>La BBE pour la liberté, l'égalité et la féminité !</p>
-                    <p>Si toi aussi tu veux faire partie de cette aventure en contribuant pour la BBE, </p>
-                    <p className='lien'>parle-moi de ton projet en  cliquant ici !</p>
+                    <p>Mon projet est de représenter la BBE en tant que model Haute Couture ainsi que de créer avec des artistes et ouvriers de lumières dans des lieux d'exceptions pour des créations en lien avec ma passion.</p>
+                    <p>Envie d'aller plus loin?</p>
+                    <p className='lien'>Clique ici et parle-moi de ton projet!</p>
                 </div>
                 <div className='image'>
                     <img src={photo3} alt='schéhérazade aux temps des coquelicots' className='photo3'></img>
                 </div>
             </section>
+            <div className='poeme'>
+            <p>"Le Moi s'élève à la grâce d'un Toi<br/>
+Ta perception de Moi est un reflet de Toi<br/>
+Ma réaction à Toi est une sensibilité en Moi"</p>
+            </div>
         </div>
     )
 }
